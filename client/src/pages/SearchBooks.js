edf20265bab4d94f38b976531180fb346791a358
tylerkandarian <tyler.kand2@gmail.com>
2023-02-18 13:56:40 -0500

    updated searchbooks

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -4,7 +4,6 @@ import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'reac
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-import { searchGoogleBooks } from '../utils/API';
 import { useMutation } from '@apollo/react-hooks';
 import {SAVE_BOOK} from '../utils/mutations';
 import { GET_ME } from '../utils/queries';
